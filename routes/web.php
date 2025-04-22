d093ebfef4a6422c69bfeee0bbe5b576ea2295dc
Oussama ELKHAMLICHI <135962512+OussamaElkamlichi@users.noreply.github.com>
2025-04-22 08:59:25 +0100

    feat(cv): cs supported by three langs

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,17 @@ Route::get('/cv/english', function () {
     return view('curriculum_vitae.eng');
 })->name('cv.english');
 
+Route::get('/cv/spanish', function () {
+    return view('curriculum_vitae.esp');
+})->name('cv.spanish');
+
+Route::get('/cv/arabic', function () {
+    return view('curriculum_vitae.ar');
+})->name('cv.arabic');
+
 Route::get('/art/chronology', function () {
     return view('art.chronology');
-})->name('art.chronology'); 
+})->name('art.chronology');
 
 Route::get('/articles', function () {
     return view('articles.index');
